9b46e2be64c0dbcb95acd6eb91723777b409abaa
Hardik Gayner <hardikgayner@gmail.com>
2025-03-23 19:23:56 +0530

    Getting user's info via token

---
+++ src/helpers/getDataFromToken.ts
@@ -0,0 +1,18 @@
+// extracting info from token
+// Getting token data
+
+import { NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
+
+export const getDataFromToken = (request: NextRequest) => {
+  try {
+    const token = request.cookies.get("token")?.value || '';
+
+    //decode/verify & extracts the info from token
+    const decodedToken:any = jwt.verify(token, process.env.TOKEN_SECRET!);
+
+    return decodedToken.id;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
